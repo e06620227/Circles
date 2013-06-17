b8da10d64802e155a8a130414f8c9ebddad4b9f3
frank <zhchf2010@gmail.com>
2013-06-17 23:41:12 +0800

    add class Individual

---
+++ Circles/src/com/circles/meta/Individual.java
@@ -0,0 +1,15 @@
+package com.circles.meta;
+
+import java.util.Map;
+
+public class Individual {
+
+	private String ID;
+	private String name;
+	private String nickName;
+	
+	private String phoneNum;
+	
+	private Map<String, Individual> friends;
+	private Map<String, Individual> closeFriends;
+}
